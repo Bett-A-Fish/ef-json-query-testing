7301e7cbbce9ef322a6e03d1bedc84598e061475
Bett-A-Fish <8015067+Bett-A-Fish@users.noreply.github.com>
2022-06-24 11:07:48 -0500

    fix date categories

--- src/ef-json-query-testing/Benchmarks/MultiSearch/DateFieldBenchmarks.cs
+++ src/ef-json-query-testing/Benchmarks/MultiSearch/DateFieldBenchmarks.cs
@@ -24,57 +24,57 @@ namespace ef_json_query_testing.Benchmarks.MultiSearch
 
 
         [Benchmark]
-        [BenchmarkCategory("json", "hascolumns", "indexed", "first", "dates")]
+        [BenchmarkCategory("json", "hascolumns", "indexed", "set1", "dates")]
         public void Indexed_set1_req_date() => Search.JsonSearch_Indexed(TestValueConstants.set1_req_date);
 
         [Benchmark]
-        [BenchmarkCategory("table", "hascolumns", "media", "first", "dates")]
+        [BenchmarkCategory("table", "hascolumns", "media", "set1", "dates")]
         public void Media_set1_req_date() => Search.TableSearch_Media(TestValueConstants.set1_req_date);
 
         [Benchmark]
-        [BenchmarkCategory("json", "nocolumns", "indexed", "first", "dates")]
+        [BenchmarkCategory("json", "nocolumns", "indexed", "set1", "dates")]
         public void Indexed_noColumns_set1_req_date() => Search.JsonSearch_Indexed_NoColumns(TestValueConstants.set1_req_date);
 
         [Benchmark]
-        [BenchmarkCategory("table", "nocolumns", "media", "first", "dates")]
+        [BenchmarkCategory("table", "nocolumns", "media", "set1", "dates")]
         public void Media_noColumns_set1_req_date() => Search.TableSearch_Media_NoColumns(TestValueConstants.set1_req_date);
 
 
 
 
         [Benchmark]
-        [BenchmarkCategory("json", "hascolumns", "indexed", "first", "dates")]
+        [BenchmarkCategory("json", "hascolumns", "indexed", "set2", "dates")]
         public void Indexed_set1_op_date() => Search.JsonSearch_Indexed(TestValueConstants.set1_op_date);
 
         [Benchmark]
-        [BenchmarkCategory("table", "hascolumns", "media", "first", "dates")]
+        [BenchmarkCategory("table", "hascolumns", "media", "set2", "dates")]
         public void Media_set1_op_date() => Search.TableSearch_Media(TestValueConstants.set1_op_date);
 
         [Benchmark]
-        [BenchmarkCategory("json", "nocolumns", "indexed", "first", "dates")]
+        [BenchmarkCategory("json", "nocolumns", "indexed", "set2", "dates")]
         public void Indexed_noColumns_set1_op_date() => Search.JsonSearch_Indexed_NoColumns(TestValueConstants.set1_op_date);
 
         [Benchmark]
-        [BenchmarkCategory("table", "nocolumns", "media", "first", "dates")]
+        [BenchmarkCategory("table", "nocolumns", "media", "set2", "dates")]
         public void Media_noColumns_set1_op_date() => Search.TableSearch_Media_NoColumns(TestValueConstants.set1_op_date);
 
 
 
 
         [Benchmark]
-        [BenchmarkCategory("json", "hascolumns", "indexed", "first", "dates")]
+        [BenchmarkCategory("json", "hascolumns", "indexed", "set3", "dates")]
         public void Indexed_set1_both_date() => Search.JsonSearch_Indexed(TestValueConstants.set1_both_date);
 
         [Benchmark]
-        [BenchmarkCategory("table", "hascolumns", "media", "first", "dates")]
+        [BenchmarkCategory("table", "hascolumns", "media", "set3", "dates")]
         public void Media_set1_both_date() => Search.TableSearch_Media(TestValueConstants.set1_both_date);
 
         [Benchmark]
-        [BenchmarkCategory("json", "nocolumns", "indexed", "first", "dates")]
+        [BenchmarkCategory("json", "nocolumns", "indexed", "set3", "dates")]
         public void Indexed_noColumns_set1_both_date() => Search.JsonSearch_Indexed_NoColumns(TestValueConstants.set1_both_date);
 
         [Benchmark]
-        [BenchmarkCategory("table", "nocolumns", "media", "first", "dates")]
+        [BenchmarkCategory("table", "nocolumns", "media", "set3", "dates")]
         public void Media_noColumns_set1_both_date() => Search.TableSearch_Media_NoColumns(TestValueConstants.set1_both_date);
     }
 }
