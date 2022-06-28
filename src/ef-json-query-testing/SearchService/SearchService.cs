01d7bf60c3080538fabf008c71186a66d0a4a957
Bett-A-Fish <8015067+Bett-A-Fish@users.noreply.github.com>
2022-06-28 12:14:41 -0500

    moving methods deemed consistently worse into the old partial

--- src/ef-json-query-testing/SearchService/SearchService.cs
+++ src/ef-json-query-testing/SearchService/SearchService.cs
@@ -22,72 +22,6 @@ namespace ef_json_query_testing
         #region JSON
 
 
-        public List<Media_Json> JsonSearch_Indexed(Dictionary<int, string> searchFields, bool throwOnNoResults = true)
-        {
-            if (searchFields == null || !searchFields.Any())
-            {
-                return new List<Media_Json>();
-            }
-
-            var tableSearchFields = new List<SearchFields>();
-
-            var fieldList = _context.DynamicFields.AsNoTracking().ToList();
-            var hasSearchField = false;
-            var searchValues = new List<string>();
-            var sqlStatement = "SELECT * FROM [dbo].[Media_Json] WHERE 1=1 ";
-            var count = 0;
-            foreach (var searchField in searchFields)
-            {
-                var field = fieldList.FirstOrDefault(f => f.DynamicFieldId == searchField.Key);
-                if (field == null)
-                {
-                    continue;
-                }
-                hasSearchField = true;
-
-                searchValues.Add(field.DataType == DataTypes.StringValue ? "%" + searchField.Value + "%" : searchField.Value);
-
-                var valueType = field.DataType.GetSqlType(Max_String_Length);
-                var thing = $"$.\"{searchField.Key}\"";
-                if (field.DataType == DataTypes.DateTimeValue)
-                {
-                    sqlStatement += " AND CONVERT(" + valueType + ", JSON_VALUE([Details], '" + thing + "'), 127)";
-                }
-                else
-                {
-                    sqlStatement += " AND CONVERT(" + valueType + ", JSON_VALUE([Details], '" + thing + "'))";
-                }
-
-                sqlStatement += field.DataType == DataTypes.StringValue ? " LIKE " : field.DataType == DataTypes.DateTimeValue ? " >= " : " = ";
-                sqlStatement += "{" + count + "}";
-
-                count++;
-            }
-
-            List<Media_Json> list;
-            if (hasSearchField)
-            {
-                var q = _context.Media_Json
-                    .FromSqlRaw(sqlStatement, searchValues.ToArray())
-                    .AsNoTracking()
-                    .OrderBy(m => m.Media_JsonId)
-                    .Take(Take_Count);
-
-                list = q.ToList();
-            }
-            else
-            {
-                list = new List<Media_Json>();
-            }
-
-            if (!list.Any() && throwOnNoResults)
-            {
-                throw new Exception("No items found");
-            }
-
-            return list;
-        }
-
         public List<Media_Json> JsonSearch_Indexed_NoColumns(Dictionary<int, string> searchFields, bool throwOnNoResults = true)
         {
             if (searchFields == null || !searchFields.Any())
@@ -169,157 +103,11 @@ namespace ef_json_query_testing
             return list;
         }
 
-        public List<Media_Json> JsonSearch_Indexed_NoColumns_StringAdjustment(Dictionary<int, string> searchFields, bool throwOnNoResults = true)
-        {
-            if (searchFields == null || !searchFields.Any())
-            {
-                return new List<Media_Json>();
-            }
-
-            var tableSearchFields = new List<SearchFields>();
-
-            var fieldList = _context.DynamicFields.AsNoTracking().ToList();
-            var hasSearchField = false;
-            var searchValues = new List<string>();
-            var sqlColumns = " [Media_JsonId] ,[UploadDate] ,[OriginalFileName] ,[FilePath] ,[CreatedDate] ,[FileSize] ,[FileWidth] ,[FileHeight] ,[Description] ,[Hold], [Details] ";
-            var sqlStatement = "SELECT " + sqlColumns + " FROM [dbo].[Media_Json] WHERE 1=1 ";
-            var count = 0;
-            foreach (var searchField in searchFields)
-            {
-                var field = fieldList.FirstOrDefault(f => f.DynamicFieldId == searchField.Key);
-                if (field == null)
-                {
-                    continue;
-                }
-                hasSearchField = true;
-
-                searchValues.Add(field.DataType == DataTypes.StringValue ? "%" + searchField.Value + "%" : searchField.Value);
-
-                var valueType = field.DataType.GetSqlType(Max_String_Length);
-                var jsonKey = $"$.\"{searchField.Key}\"";
-                var jsonValue = " JSON_VALUE([Details], '" + jsonKey + "') ";
-                if (field.DataType == DataTypes.DateTimeValue)
-                {
-                    sqlStatement += " AND CONVERT(" + valueType + ", " + jsonValue + ", 127)";
-                }
-                else if (!field.IsRequired && field.DataType == DataTypes.StringValue)
-                {
-                    sqlStatement += " AND " + jsonValue + " IS NOT NULL ";
-                    sqlStatement += " AND CONVERT(" + valueType + ", " + jsonValue + ")";
-                }
-                else
-                {
-                    sqlStatement += " AND CONVERT(" + valueType + ", " + jsonValue + ")";
-                }
-
-
-                sqlStatement += field.DataType == DataTypes.StringValue ? " LIKE " : field.DataType == DataTypes.DateTimeValue ? " >= " : " = ";
-                sqlStatement += "{" + count + "}";
-
-                count++;
-            }
-
-            List<Media_Json> list;
-            if (hasSearchField)
-            {
-                var q = _context.Media_Json
-                    .FromSqlRaw(sqlStatement, searchValues.ToArray())
-                    .AsNoTracking()
-                    .Select(q => new Media_Json()
-                    {
-                        Media_JsonId = q.Media_JsonId,
-                        UploadDate = q.UploadDate,
-                        OriginalFileName = q.OriginalFileName,
-                        FilePath = q.FilePath,
-                        CreatedDate = q.CreatedDate,
-                        FileSize = q.FileSize,
-                        FileWidth = q.FileWidth,
-                        FileHeight = q.FileHeight,
-                        Description = q.Description,
-                        Hold = q.Hold
-                    })
-                    .OrderBy(m => m.Media_JsonId)
-                    .Take(Take_Count);
-
-                list = q.ToList();
-            }
-            else
-            {
-                list = new List<Media_Json>();
-            }
-
-            if (!list.Any() && throwOnNoResults)
-            {
-                throw new Exception("No items found");
-            }
-
-            return list;
-        }
-
         #endregion
 
 
         #region Dynamic Table Store
 
-
-        public List<Media_Dynamic> TableSearch_Media(Dictionary<int, string> searchFields, bool throwOnNoResults = true)
-        {
-            if (searchFields == null || !searchFields.Any())
-            {
-                return new List<Media_Dynamic>();
-            }
-
-            _context.Database.SetCommandTimeout(500);
-            var fieldList = _context.DynamicFields.AsNoTracking().ToList();
-            var query = _context.Media_Dynamic.AsNoTracking().Include(d => d.DynamicMediaInformation).AsQueryable();
-            var hasSearchField = false;
-            foreach (var searchField in searchFields)
-            {
-                var field = fieldList.FirstOrDefault(f => f.DynamicFieldId == searchField.Key);
-                if (field == null)
-                {
-                    continue;
-                }
-                hasSearchField = true;
-
-                if (field.DataType == DataTypes.StringValue)
-                {
-                    query = query.Where(m => m.DynamicMediaInformation.Any(i => i.FieldId == field.DynamicFieldId && i.Value.Contains(searchField.Value)));
-                }
-                else if (field.DataType == DataTypes.BoolValue)
-                {
-                    var val = ConvertBoolean(searchField.Value);
-                    query = query.Where(m => m.DynamicMediaInformation.Any(i => i.FieldId == field.DynamicFieldId && i.Value == val));
-                }
-                else if (field.DataType == DataTypes.DateTimeValue)
-                {
-
-                    var val = DateTime.Parse(searchField.Value);
-                    query = query.Where(m => m.DynamicMediaInformation.Any(i => i.FieldId == field.DynamicFieldId && EF.Functions.DateConvert(i.Value) >= val));
-                }
-                else
-                {
-                    query = query.Where(m => m.DynamicMediaInformation.Any(i => i.FieldId == field.DynamicFieldId && i.Value == searchField.Value));
-                }
-            }
-
-            List<Media_Dynamic> list;
-            if (hasSearchField)
-            {
-                list = query.OrderBy(q => q.Media_DynamicId).Take(Take_Count).ToList();
-            }
-            else
-            {
-                list = new List<Media_Dynamic>();
-            }
-
-            if (!list.Any() && throwOnNoResults)
-            {
-                throw new Exception("No items found");
-            }
-
-            return list;
-        }
 
         public List<Media_Dynamic> TableSearch_Media_NoColumns(Dictionary<int, string> searchFields, bool throwOnNoResults = true)
         {
@@ -395,15 +183,8 @@ namespace ef_json_query_testing
 
     public partial interface ISearchService
     {
-        List<Media_Json> JsonSearch_Indexed(Dictionary<int, string> searchFields, bool throwOnNoResults = true);
-
         List<Media_Json> JsonSearch_Indexed_NoColumns(Dictionary<int, string> searchFields, bool throwOnNoResults = true);
         
-        List<Media_Json> JsonSearch_Indexed_NoColumns_StringAdjustment(Dictionary<int, string> searchFields, bool throwOnNoResults = true);
-
-
-
-        List<Media_Dynamic> TableSearch_Media(Dictionary<int, string> searchFields, bool throwOnNoResults = true);
 
         List<Media_Dynamic> TableSearch_Media_NoColumns(Dictionary<int, string> searchFields, bool throwOnNoResults = true);
     }
